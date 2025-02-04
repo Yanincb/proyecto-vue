345464e13f748632790732b224a8d90b505b7546
YANIN\Yanin <a23311041@uthermosillo.edu.mx>
2025-02-04 12:37:54 -0700

    Crea componente Lista De Tareas.Borra componentes autogenerados.Crea rutas

---
+++ src/views/MainView.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="container">
+    <Sidebar> </Sidebar>
+    <div class="content">
+        <Router-View></Router-View>
+    </div>
+</div>
+
+</template>
+<script setup>
+import Sidebar from '@/components/Sidebar.vue';
+</script>
+<style>
+.container{
+    display: flex;
+}
+.container{
+    flex: 1;
+    padding: 20px;
+}
+
+</style>
