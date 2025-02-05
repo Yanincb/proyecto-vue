eccc7c88a976ba5b344feb8bd6bc440c2667f65a
YANIN\Yanin <a23311041@uthermosillo.edu.mx>
2025-02-04 19:56:36 -0700

    Componentes Padres, Componentes Hijos y Props

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,14 +1,20 @@
 <template>
+    <Header :mensaje="texto"></Header>
 <div class="container">
     <Sidebar> </Sidebar>
     <div class="content">
-        <Router-View></Router-View>
+        <input type="texto" v-model="texto">
+        {{ texto }}
+        <router-view></router-view>
     </div>
 </div>
 
 </template>
 <script setup>
 import Sidebar from '@/components/Sidebar.vue';
+import Header from '@/components/Header.vue';
+import {ref} from 'vue';
+const texto = ref('');
 </script>
 <style>
 .container{
